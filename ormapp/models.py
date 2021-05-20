a1cbdabbabd411bc4dd9f17ddbe9e2a24f58cff9
nildiert <niljordan23@gmail.com>
2021-05-20 13:59:42 -0500

    added vehicle class

--- ormapp/models.py
+++ ormapp/models.py
@@ -4,3 +4,14 @@ from django.db import models
 class Customer(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+class Vehicle(models.Model):
+    name = models.CharField(max_length=255)
+    customer = models.OneToOneField(
+        Customer,
+        on_delete=models.CASCADE,
+        related_name='vehicle'
+    )
